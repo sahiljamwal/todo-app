00436f0da7c18ceb768389dcde124a5426d9ce61
Sahil Jamwal <sahil.jamwal78625@gmail.com>
2020-04-26 16:34:24 +0530

    Updates

---
+++ precache-manifest.7516b305dcd955633b9e9b716f8d7acb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffffc911510fe9ab4a50d7bee3b82118",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "6cad496355a3c0a723e9",
+    "url": "/todo-app/static/css/main.3dfa90a5.chunk.css"
+  },
+  {
+    "revision": "95601bee32bc8420c8d5",
+    "url": "/todo-app/static/js/2.1663555d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.1663555d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cad496355a3c0a723e9",
+    "url": "/todo-app/static/js/main.a1838b6d.chunk.js"
+  },
+  {
+    "revision": "e95f3875b6971c7c64a6",
+    "url": "/todo-app/static/js/runtime-main.eeaa846d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
